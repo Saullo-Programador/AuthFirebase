1feb100c2019f613de33194a00530caaa0dd6c67
Saullo-Programador <saullopaulodantasfelipe8@gmail.com>
2024-04-06 17:13:32 -0300

    start Project

--- src/Screens/SignInScreen.tsx
+++ src/Screens/SignInScreen.tsx
@@ -1,42 +1,42 @@
-import React, {useState} from 'react';
-import {View, Image} from 'react-native';
+import React, { useState } from 'react';
+import { View, Image } from 'react-native';
 import auth from '@react-native-firebase/auth'
-import {styles} from './Styles';
-import {MyButton} from '../Components/MyButton';
-import {MyTextInput} from '../Components/MyTextInput';
-import {MyLink} from '../Components/MyLInk';
+import { styles } from './Styles';
+import { MyButton } from '../Components/MyButton';
+import { MyTextInput } from '../Components/MyTextInput';
+import { MyLink } from '../Components/MyLInk';
 
 export function SignInScreen() {
   const logo = require('../Assets/logo.png')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('');
 
-  function signUp(){
-    auth().createUserWithEmailAndPassword(email,password)
-    .then(userCredencial=>{
-      console.log("User: ", userCredencial);
-    }).catch(error => {
-      if(error.code === 'auth/email-already-in-use'){
-        console.log('email já existe');
-      }
-      if (error.code === 'auth/invalid-email'){
-        console.log('email inválido');
-      }
-    });
+  function signUp() {
+    auth().createUserWithEmailAndPassword(email, password)
+      .then(userCredencial => {
+        console.log("User: ", userCredencial);
+      }).catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('email já existe');
+        }
+        if (error.code === 'auth/invalid-email') {
+          console.log('email inválido');
+        }
+      });
   }
 
-  function signIn(){
+  function signIn() {
     auth().createUserWithEmailAndPassword(email, password)
-    .then(() => console.log('usuario logado')).catch(error => {
-      if (error.code === 'auth/email-already-in-use'){
-        console.log('senha inválida');
-      }
-    });
+      .then(() => console.log('usuario logado')).catch(error => {
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('senha inválida');
+        }
+      });
   }
 
   return (
-    <View style={[styles.container, {justifyContent: 'center'}]}>
-      <Image resizeMode="contain" source={logo} style={{width: 200}} />
+    <View style={[styles.container, { justifyContent: 'center' }]}>
+      <Image resizeMode="contain" source={logo} style={{ width: 200 }} />
       <MyTextInput placeholder="E-mail" value={email} onChangeText={setEmail} />
       <MyTextInput
         placeholder="Senha"
@@ -47,7 +47,7 @@ export function SignInScreen() {
 
       <MyButton title="Entrar no App" onPress={signIn} />
 
-      <MyLink title="Cadastrar" onPress={signUp}/>
+      <MyLink title="Cadastrar" onPress={signUp} />
     </View>
   );
 }
